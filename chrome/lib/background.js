3320406021516960794439830a3d4364b047fcf5
Eran Boudjnah <eranbou@gmail.com>
2023-05-12 11:56:13 +0100

    Added Twitter support. Some tidy up.

--- chrome/lib/background.js
+++ chrome/lib/background.js
@@ -1,16 +1,20 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (!tab.url) return undefined;
     if (tab.url.startsWith("chrome://") || tab.url.startsWith("edge://")) return undefined;
-    if (!tab.url.match(/https?:\/\/([a-zA-Z0-0]+\.)*(facebook|linkedin)\.com/)) {
+    if (!tab.url.match(/https?:\/\/([a-zA-Z0-0]+\.)*(facebook|linkedin|twitter)\.com/)) {
         return undefined;
     }
 
     if (tab.active && changeInfo.status == 'complete') {
-        chrome.scripting.executeScript(
-            {
-                target: {tabId: tabId, allFrames: true},
-                files: ['lib/fontify.js'],
+        chrome.tabs.sendMessage(tabId, 'ping', {frameId: 0}, () => {
+            if (chrome.runtime.lastError) {
+                chrome.scripting.executeScript(
+                    {
+                        target: {tabId: tabId, allFrames: true},
+                        files: ['lib/fontify.js'],
+                    }
+                );
             }
-        );
+        });
     }
 });
