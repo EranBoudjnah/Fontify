8d6cad26a8ae1ae0fd113bbe2bcf8e5848615adf
Eran Boudjnah <eranbou@gmail.com>
2026-04-12 10:53:41 +0100

    Added lint checks. (#5)

--- chrome/lib/background.js
+++ chrome/lib/background.js
@@ -1,11 +1,11 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (!tab.url) return undefined;
     if (tab.url.startsWith("chrome://") || tab.url.startsWith("edge://")) return undefined;
     if (!tab.url.match(/https?:\/\/([a-zA-Z0-0]+\.)*(facebook|linkedin|twitter)\.com/)) {
         return undefined;
     }
 
-    if (tab.active && changeInfo.status == 'complete') {
+    if (tab.active && changeInfo.status === 'complete') {
         chrome.tabs.sendMessage(tabId, 'ping', {frameId: 0}, () => {
             if (chrome.runtime.lastError) {
                 chrome.scripting.executeScript(
